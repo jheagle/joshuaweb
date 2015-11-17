51f380aaa5b2a1083745a4a0d89b437f4d989ebe
Joshua Heagle <joshuaheagle@gmail.com>
2015-11-16 22:24:40 -0500

    Throw out excess images
    Remove the parallax concept of the Toronto scene (with the bad quality
    picture) and replace with a clean one found online. Remove all other
    images that will not be used as this time.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-    var $cnTower = $('#cn-tower');
-    var $foreBuildings = $('#fore-buildings');
-    var $midBuildings = $('#mid-buildings');
-    var $boat = $('#boat');
-    var $bushes = $('#bushes');
+//    var $cnTower = $('#cn-tower');
+//    var $foreBuildings = $('#fore-buildings');
+//    var $midBuildings = $('#mid-buildings');
+//    var $boat = $('#boat');
+//    var $bushes = $('#bushes');
     var lastId, lastHref = '#', topMenu = $(".menu"), menuItems = topMenu.find("a"), scrollItems = menuItems.map(function () {
         var item = $($(this).attr("href"));
         if (item.length) {
@@ -34,34 +34,34 @@ $(document).ready(function () {
         });
         cur = cur[cur.length - 1];
         var id = cur && cur.length ? cur[0].id : "";
-        if (id === 'intro' || id === 'profile') {
-            var opacity = 0;
-            var bushesPos = 33, boatPos = 40, foreBuildPos = 20, midBuildPos = 13, cnPos = 7;
-            if (fromTop < mainHeight) {
-                opacity = fromTop / mainHeight;
-            }
-            if (fromTop >= mainHeight && fromTop <= mainHeight) {
-                opacity = 1;
-            }
-            if (fromTop > mainHeight) {
-                opacity = 1 - ((fromTop - mainHeight) / mainHeight);
-            }
-            if (opacity !== lastOpacity) {
-                lastOpacity = opacity;
-                cnPos *= opacity;
-                foreBuildPos *= opacity;
-                midBuildPos *= opacity;
-                boatPos *= opacity;
-                bushesPos *= opacity;
-
-                $('#profile').find('.content').css('opacity', opacity);
-                $cnTower.css('left', '-' + cnPos + '%');
-                $foreBuildings.css('left', '-' + foreBuildPos + '%');
-                $midBuildings.css('left', '-' + midBuildPos + '%');
-                $boat.css('left', '-' + boatPos + '%');
-                $bushes.css('left', '-' + bushesPos + '%');
-            }
-        }
+//        if (id === 'intro' || id === 'profile') {
+//            var opacity = 0;
+//            var bushesPos = 33, boatPos = 40, foreBuildPos = 20, midBuildPos = 13, cnPos = 7;
+//            if (fromTop < mainHeight) {
+//                opacity = fromTop / mainHeight;
+//            }
+//            if (fromTop >= mainHeight && fromTop <= mainHeight) {
+//                opacity = 1;
+//            }
+//            if (fromTop > mainHeight) {
+//                opacity = 1 - ((fromTop - mainHeight) / mainHeight);
+//            }
+//            if (opacity !== lastOpacity) {
+//                lastOpacity = opacity;
+//                cnPos *= opacity;
+//                foreBuildPos *= opacity;
+//                midBuildPos *= opacity;
+//                boatPos *= opacity;
+//                bushesPos *= opacity;
+//
+//                $('#profile').find('.content').css('opacity', opacity);
+//                $cnTower.css('left', '-' + cnPos + '%');
+//                $foreBuildings.css('left', '-' + foreBuildPos + '%');
+//                $midBuildings.css('left', '-' + midBuildPos + '%');
+//                $boat.css('left', '-' + boatPos + '%');
+//                $bushes.css('left', '-' + bushesPos + '%');
+//            }
+//        }
         if (lastId !== id) {
             lastId = id;
             menuItems.parent().removeClass("current").end().filter("[href=#" + id + "]").parent().addClass("current");
