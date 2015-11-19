a645b9693ff57590d0d311be441b67538eb85efe
Joshua Heagle <joshuaheagle@gmail.com>
2015-11-18 20:44:15 -0500

    scrollTop Problems in WebKit

--- js/main.js
+++ js/main.js
@@ -1,30 +1,26 @@
 $(document).ready(function () {
-//    var $cnTower = $('#cn-tower');
-//    var $foreBuildings = $('#fore-buildings');
-//    var $midBuildings = $('#mid-buildings');
-//    var $boat = $('#boat');
-//    var $bushes = $('#bushes');
-    var lastId, lastHref = '#', topMenu = $(".menu"), menuItems = topMenu.find("a"), scrollItems = menuItems.map(function () {
+    var lastId, topMenu = $(".menu"), menuItems = topMenu.find("a"), scrollItems = menuItems.map(function () {
         var item = $($(this).attr("href"));
         if (item.length) {
             return item;
         }
     });
     menuItems.click(function (e) {
-        e.preventDefault();
+//        var href = $(this).attr("href"), offset = href === "#" ? 0 : $(href).offset().top + 1;
         var index = $(this).parent().index() - 1;
         var mainHeight = $('#intro').outerHeight();
-        var href = $(this).attr('href');
-        var offset = 0;
         offset = mainHeight * index;
         $('html, body').stop().animate({
             scrollTop: offset
         }, 550);
+        menuItems.parent().removeClass("current");
+        $(this).parent().addClass("current");
+        e.preventDefault();
     });
 
     var lastOpacity = 0;
 
-    $('html,body').scroll(function () {
+    $(window).scroll(function () {
         var mainHeight = $('#intro').outerHeight();
         var fromTop = $(this).scrollTop() + 10, cur = scrollItems.map(function () {
             var index = $(this).index() - 2;
