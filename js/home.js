4f87f90dec1f1fb09c8ef1a9a5cabfd4a2c50e0d
Joshua Heagle <joshuaheagle@gmail.com>
2016-02-07 22:36:11 -0500

    Added most images for Work page

--- js/home.js
+++ js/home.js
@@ -1,13 +1,16 @@
-(function ($) {
-  $(document).ready(function () {
-    var lastId, topMenu = $(".menu"), menuItems = topMenu.find("a"), scrollItems = menuItems.map(function () {
-      var item = $($(this).attr("href"));
-      if (item.length) {
-        return item;
-      }
-    });
-    menuItems.click(function (e) {
-      var href = $(this).attr("href"), offset = href === "#" ? 0 : $(href).offset().top + 1;
+(function($) {
+  $(document).ready(function() {
+    var lastId, topMenu = $(".menu"),
+      menuItems = topMenu.find("a"),
+      scrollItems = menuItems.map(function() {
+        var item = $($(this).attr("href"));
+        if (item.length) {
+          return item;
+        }
+      });
+    menuItems.click(function(e) {
+      var href = $(this).attr("href"),
+        offset = href === "#" ? 0 : $(href).offset().top + 1;
       $('html, body').stop().animate({
         scrollTop: offset
       }, 550);
@@ -16,19 +19,76 @@
       e.preventDefault();
     });
 
-    $(window).scroll(function () {
-      var fromTop = $(this).scrollTop() + 10, cur = scrollItems.map(function () {
-        if ($(this).offset().top < fromTop)
-        return this;
-      });
+    $(window).scroll(function() {
+      var fromTop = $(this).scrollTop() + 10,
+        cur = scrollItems.map(function() {
+          if ($(this).offset().top < fromTop)
+            return this;
+        });
       cur = cur[cur.length - 1];
       var id = cur && cur.length ? cur[0].id : "";
       if (lastId !== id) {
         lastId = id;
         if (location.hash !== '#' + id)
-        window.location.hash = id;
+          window.location.hash = id;
         menuItems.parent().removeClass("current").end().filter("[href=#" + id + "]").parent().addClass("current");
       }
     });
   });
 })(jQuery);
+var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+var isIE = /*@cc_on!@*/ false;
+var isFirefox = typeof InstallTrigger !== 'undefined';
+var camTitle = document.getElementById('cam-title');
+if (!isSafari && !isIE && !isFirefox) {
+  var camera = Math.floor(Math.random() * 12);
+  switch (camera) {
+    case 0:
+      document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=41');} </style>");
+      camTitle.innerHTML = "Albert &amp; Lyon";
+      break;
+    case 1:
+      document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=42');} </style>");
+      camTitle.innerHTML = "Albert &amp; O'Connor";
+      break;
+    case 2:
+      document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=123');} </style>");
+      camTitle.innerHTML = "Bank &amp; Laurier";
+      break;
+    case 3:
+      document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=64');} </style>");
+      camTitle.innerHTML = "Bank &amp; Somerset";
+      break;
+    case 4:
+      document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=34');} </style>");
+      camTitle.innerHTML = "Elgin &amp; Queen";
+      break;
+    case 5:
+      document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=157');} </style>");
+      camTitle.innerHTML = "Bay &amp; Laurier";
+      break;
+    case 6:
+      document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=184');} </style>");
+      camTitle.innerHTML = "Besserer &amp; Waller";
+      break;
+    case 7:
+      document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=4');} </style>");
+      camTitle.innerHTML = "Bronson &amp; Carling";
+      break;
+    case 8:
+      document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=185');} </style>");
+      camTitle.innerHTML = "Dalhousie &amp; Murray";
+      break;
+    case 9:
+      document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=107');} </style>");
+      camTitle.innerHTML = "Dalhousie &amp; Rideau";
+      break;
+    case 10:
+      document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=46');} </style>");
+      camTitle.innerHTML = "Kent &amp; Slater";
+      break;
+    default:
+      document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=189');} </style>");
+      camTitle.innerHTML = "Bank &amp; Fifth";
+  }
+}
--- js/title-switcher.js
+++ js/title-switcher.js
@@ -1,4 +1,4 @@
-(function ($) {
+(function($) {
   // Created by Joshua Web Strategy
 
   /*********************************************
@@ -10,7 +10,7 @@
   **********************************************/
   var currentClass = 'displayTitle';
   var titles = [];
-  var TitleSwitcher = function (currentClass, titlesContainer) {
+  var TitleSwitcher = function(currentClass, titlesContainer) {
     this.currentClass = currentClass;
     this.titlesContainer = titlesContainer;
     this.titles = titlesContainer.children();
@@ -19,7 +19,7 @@
     this.delaySwitch = 400;
     this.delayEffect = 200;
 
-    this.startTitles = function (isRandom, delaySwitch, delayEffect) {
+    this.startTitles = function(isRandom, delaySwitch, delayEffect) {
       var typeSurface = 'typeSurface';
       var currentIndex = this.titlesContainer.find('.' + this.currentClass).index();
       var currentTitle = this.titles[currentIndex];
@@ -33,61 +33,61 @@
       $self.switchTitle(currentTitle, $self.typingEffect, this);
     };
 
-    this.cursorBlink = function (blinkOn, self) { // display cursor effect
+    this.cursorBlink = function(blinkOn, self) { // display cursor effect
       var $self = self || this;
-      if (blinkOn){
-        $self.typeSurface.html($self.typeSurface.html().replace('|', ''));
+      if (blinkOn) {
+        $self.typeSurface.html($.trim($self.typeSurface.html().replace('/^(.*)*\||&nbsp;$/', '')));
         $self.typeSurface.html($self.typeSurface.html() + '<span style="font-weight: normal; color: black; text-decoration: none">&#124;</span>');
-      }else{
-        $self.typeSurface.html($self.typeSurface.html().replace('|', ''));
+      } else {
+        $self.typeSurface.html($self.typeSurface.html().replace('|', '&nbsp;'));
       };
     };
 
-    this.typingEffect = function (domObject, callBackFunction, self) {
+    this.typingEffect = function(domObject, callBackFunction, self) {
       var $self = self || this;
       var currentIndex = $self.titlesContainer.find('.' + $self.currentClass).index();
-      var domObject = domObject || $self.titles[currentIndex+1];
+      var domObject = domObject || $self.titles[currentIndex + 1];
       var callBackFunction = callBackFunction || $self.typingEffect;
       var blinkOn = true;
       var numBlinks = 4;
-      if ($self.typeSurface.attr('style')){
+      if ($self.typeSurface.attr('style')) {
         $self.typeSurface.removeAttr('style');
       }
-      if ($(domObject).attr('style')){
+      if ($(domObject).attr('style')) {
         $self.typeSurface.attr('style', $(domObject).attr('style'));
       }
       $self.typeSurface.empty().show();
 
-      for (var i = 0; i < numBlinks; ++i){
-        setTimeout(function () {
+      for (var i = 0; i < numBlinks; ++i) {
+        setTimeout(function() {
           $self.cursorBlink(blinkOn, $self);
           blinkOn = !blinkOn;
-        }, i*$self.delaySwitch);
+        }, i * $self.delaySwitch);
       }
 
-      setTimeout(function () {
+      setTimeout(function() {
         $self.typeSurface.empty();
         $self.typeSurface.html($self.typeSurface.html() + '<span style="font-weight: normal; color: black; text-decoration: none">&#124;</span>');
-        $.each($(domObject).text().split(''), function (i, letter) {
-          setTimeout(function () {
+        $.each($(domObject).text().split(''), function(i, letter) {
+          setTimeout(function() {
             $self.typeSurface.html($self.typeSurface.text().replace('|', '') + letter + '<span style="font-weight: normal; color: black; text-decoration: none">&#124;</span>');
             // Replace html with old html on last letter
             if ($(domObject).text() + '|' === $self.typeSurface.text()) {
               $self.typeSurface.html(domObject.innerHTML + '<span style="font-weight: normal; color: black; text-decoration: none">&#124;</span>');
-              for (var j = 0; j < numBlinks*2; ++j){
-                setTimeout(function () {
+              for (var j = 0; j < numBlinks * 2; ++j) {
+                setTimeout(function() {
                   --j;
                   $self.cursorBlink(blinkOn, $self);
                   blinkOn = !blinkOn;
-                  if(j === 0){
+                  if (j === 0) {
                     callBackFunction(domObject, $self.typingEffect, $self);
                   }
-                }, j*$self.delaySwitch);
+                }, j * $self.delaySwitch);
               }
             }
-          }, i*$self.delayEffect);
+          }, i * $self.delayEffect);
         });
-      }, numBlinks*$self.delaySwitch);
+      }, numBlinks * $self.delaySwitch);
     };
 
     this.switchTitle = function(currentTitle, callBackFunction, self) {
@@ -95,21 +95,21 @@
       var currentIndex = $(currentTitle).index();
       var maxIndex = $self.titles.length;
       var nextIndex = 0;
-      if (maxIndex === 1){
+      if (maxIndex === 1) {
         return callBackFunction(currentTitle, $self.switchTitle, $self);
       }
-      if ($self.isRandom){
-        if (!$.trim($self.typeSurface.text())){
+      if ($self.isRandom) {
+        if (!$.trim($self.typeSurface.text())) {
           currentIndex = -1;
         }
         do {
-          nextIndex = Math.round(Math.random() * maxIndex-1);
-        }while (nextIndex === currentIndex-1 || nextIndex >= maxIndex);
-      }else{
-        if (!$.trim($self.typeSurface.text())){
+          nextIndex = Math.round(Math.random() * maxIndex - 1);
+        } while (nextIndex === currentIndex - 1 || nextIndex >= maxIndex);
+      } else {
+        if (!$.trim($self.typeSurface.text())) {
           currentIndex = maxIndex;
         }
-        nextIndex = currentIndex < maxIndex? currentIndex: 0;
+        nextIndex = currentIndex < maxIndex ? currentIndex : 0;
       }
       var nextTitle = $self.titles[nextIndex];
       $(currentTitle).removeClass($self.currentClass);
@@ -117,15 +117,15 @@
       return callBackFunction(nextTitle, $self.switchTitle, $self);
     };
   }
-  
-  $('.' + currentClass).each(function(i, titleClass){
+
+  $('.' + currentClass).each(function(i, titleClass) {
     var title = new TitleSwitcher(currentClass, $(titleClass).parent());
     titles.push(title);
   });
-  $.each(titles, function(i, title){
-    if (title.titlesContainer.hasClass('title-switcher')){
+  $.each(titles, function(i, title) {
+    if (title.titlesContainer.hasClass('title-switcher')) {
       title.startTitles(false, 400, 100);
-    }else{
+    } else {
       title.startTitles(true);
     }
   });
