9527a5e2d9f06eaf45a64ea26288e7981d661b29
Joshua Heagle <joshuaheagle@gmail.com>
2016-05-03 09:17:42 -0400

    formatting

--- js/home.js
+++ js/home.js
@@ -1,7 +1,7 @@
 (function($) {
   var lastId, topMenu = $(".menu"),
-  menuItems = topMenu.find("a"),
-  menuItemsTemp = [];
+    menuItems = topMenu.find("a"),
+    menuItemsTemp = [];
   $.each(menuItems, function(i, data) {
     if (i !== 3) {
       menuItemsTemp.push(data);
@@ -17,16 +17,16 @@
   menuItemsTemp = null;
   menuItems.click(function(e) {
     var href = $(this).attr("href"),
-    offset = href === "#" ? 0 : $(href).offset().top + 1;
+      offset = href === "#" ? 0 : $(href).offset().top + 1;
     $('html, body').stop().animate({
-      scrollTop: offset
-    },
-    550,
-    function() {
-      if (location.hash !== href)
-      adjustMain(href);
-      window.location.hash = href;
-    });
+        scrollTop: offset
+      },
+      550,
+      function() {
+        if (location.hash !== href)
+          adjustMain(href);
+        window.location.hash = href;
+      });
     menuItems.parent().removeClass("current");
     $(this).parent().addClass("current");
     e.preventDefault();
@@ -34,10 +34,10 @@
 
   $(window).scroll(function() {
     var fromTop = $(this).scrollTop() + 10,
-    cur = scrollItems.map(function() {
-      if ($(this).offset().top < fromTop)
-      return this;
-    });
+      cur = scrollItems.map(function() {
+        if ($(this).offset().top < fromTop)
+          return this;
+      });
     cur = cur[cur.length - 1];
     var id = cur && cur.length ? cur[0].id : "";
     if (lastId !== id) {
@@ -70,8 +70,8 @@
     $('.error-status').removeClass('error-status');
     $(".error-msg").remove();
     var error = false,
-    phoneError = "",
-    tel = 0;
+      phoneError = "",
+      tel = 0;
     if ($("#name").val() === "") {
       error = true;
       $("#name").addClass("error-status");
@@ -140,51 +140,51 @@ if (!isSafari && !isIE && !isFirefox) {
   var camera = Math.floor(Math.random() * 12);
   switch (camera) {
     case 0:
-    document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=41');} </style>");
-    camTitle.innerHTML = "Albert &amp; Lyon";
-    break;
+      document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=41');} </style>");
+      camTitle.innerHTML = "Albert &amp; Lyon";
+      break;
     case 1:
-    document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=42');} </style>");
-    camTitle.innerHTML = "Albert &amp; O'Connor";
-    break;
+      document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=42');} </style>");
+      camTitle.innerHTML = "Albert &amp; O'Connor";
+      break;
     case 2:
-    document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=123');} </style>");
-    camTitle.innerHTML = "Bank &amp; Laurier";
-    break;
+      document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=123');} </style>");
+      camTitle.innerHTML = "Bank &amp; Laurier";
+      break;
     case 3:
-    document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=64');} </style>");
-    camTitle.innerHTML = "Bank &amp; Somerset";
-    break;
+      document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=64');} </style>");
+      camTitle.innerHTML = "Bank &amp; Somerset";
+      break;
     case 4:
-    document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=34');} </style>");
-    camTitle.innerHTML = "Elgin &amp; Queen";
-    break;
+      document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=34');} </style>");
+      camTitle.innerHTML = "Elgin &amp; Queen";
+      break;
     case 5:
-    document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=157');} </style>");
-    camTitle.innerHTML = "Bay &amp; Laurier";
-    break;
+      document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=157');} </style>");
+      camTitle.innerHTML = "Bay &amp; Laurier";
+      break;
     case 6:
-    document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=184');} </style>");
-    camTitle.innerHTML = "Besserer &amp; Waller";
-    break;
+      document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=184');} </style>");
+      camTitle.innerHTML = "Besserer &amp; Waller";
+      break;
     case 7:
-    document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=4');} </style>");
-    camTitle.innerHTML = "Bronson &amp; Carling";
-    break;
+      document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=4');} </style>");
+      camTitle.innerHTML = "Bronson &amp; Carling";
+      break;
     case 8:
-    document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=185');} </style>");
-    camTitle.innerHTML = "Dalhousie &amp; Murray";
-    break;
+      document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=185');} </style>");
+      camTitle.innerHTML = "Dalhousie &amp; Murray";
+      break;
     case 9:
-    document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=107');} </style>");
-    camTitle.innerHTML = "Dalhousie &amp; Rideau";
-    break;
+      document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=107');} </style>");
+      camTitle.innerHTML = "Dalhousie &amp; Rideau";
+      break;
     case 10:
-    document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=46');} </style>");
-    camTitle.innerHTML = "Kent &amp; Slater";
-    break;
+      document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=46');} </style>");
+      camTitle.innerHTML = "Kent &amp; Slater";
+      break;
     default:
-    document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=189');} </style>");
-    camTitle.innerHTML = "Bank &amp; Fifth";
+      document.write("<style>#live-cam { background: url('http://traffic.ottawa.ca/map/camera?id=189');} </style>");
+      camTitle.innerHTML = "Bank &amp; Fifth";
   }
 }
